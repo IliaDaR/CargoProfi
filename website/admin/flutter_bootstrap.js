97d962617ab93d5d51caca31be0ef3b173e6b16a
CargoProfi Dev <user@cargoprofi.dev>
2026-05-30 10:33:37 +0300

    Restore full app, serve via HTTP (file:// blocks Flutter Web)

--- website/admin/flutter_bootstrap.js
+++ website/admin/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2770478068" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2881233863" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
