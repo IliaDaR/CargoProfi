85b2705a69ece2104078d5ed8856eff0c11cd67e
CargoProfi Dev <user@cargoprofi.dev>
2026-05-30 10:04:07 +0300

    Deploy admin build with lazy Firebase init

--- website/admin/flutter_bootstrap.js
+++ website/admin/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1339717633" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3084133901" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
