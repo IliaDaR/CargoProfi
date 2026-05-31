8feaf4a0d4b076f0083fad47effbe64bcc3c7190
IliaDaR <IliaDaR@users.noreply.github.com>
2026-05-31 07:23:36 +0000

    deploy: 3735eef5fbfc27a00ae6c2792a7be916e3a6309b

--- server.bat
+++ server.bat
@@ -2,12 +2,11 @@
 cd /d "D:\CargoProfi\website"
 echo.
 echo ========================================
-echo   Numino Server — http://localhost:8090
+echo   Numino Server — http://localhost:3000
 echo   Нажми Ctrl+C чтобы остановить
 echo ========================================
-echo.
 :loop
-python -m http.server 8090
+python -m http.server 3000
 echo Сервер упал. Перезапуск через 3 сек...
 timeout /t 3 >nul
 goto loop
